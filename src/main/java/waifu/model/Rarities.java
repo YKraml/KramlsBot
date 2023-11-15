96e6620179ef35f6c8ae42a63b08864f01d002ff
Yannick Kraml <82815144+YKraml@users.noreply.github.com>
2023-11-15 14:46:10 +0100

    Update Rarities.java

--- src/main/java/waifu/model/Rarities.java
+++ src/main/java/waifu/model/Rarities.java
@@ -8,7 +8,7 @@ public enum Rarities {
   RARE("Rare", 25, 1000, 150, 0.50, Color.GREEN, 0),
   EPIC("Epic", 50, 10000, 200, 0.15, Color.MAGENTA, 500),
   LEGENDARY("Legendary", 80, 100000, 300, 0.02, Color.YELLOW, 5000),
-  MYTHICAL("Mystisch", 100, Integer.MAX_VALUE, 400, 0, Color.WHITE, 5000);
+  MYTHICAL("Mythical", 100, Integer.MAX_VALUE, 400, 0, Color.WHITE, 5000);
 
   private final String name;
   private final int minLevel;
